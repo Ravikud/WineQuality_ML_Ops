6d2adfca84960fe814762a135d802fd7d53eef0c
Ravikud <ravinampad@gmail.com>
2021-03-31 22:34:58 +0530

    Commit Files

--- test/test_config.py
+++ test/test_config.py
@@ -1,6 +1,14 @@
+import pytest
+
+class NotInRange(Exception):
+def __init__(self,message="value not in range"):
+    self.message=message
+    #Inheriting from init constructor
+    super().__init__(self.message)
+
 
 def test_generic():
-    a=2
-    b=2
-
-    assert a == b
+    a=5
+    with pytest.raises(NotInRange):
+        if a NotInRange(10,20):
+            raise NotInRange
